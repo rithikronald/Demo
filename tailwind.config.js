cac5014a81b3466eb3db59fbc0b290f20e4b897c
Dikshit Upadhya <diksupadhya1234@gmail.com>
2022-08-09 07:55:49 +0530

    add sidebar and styles add configured routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
+  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens:{
       sm:'480px',
@@ -23,5 +24,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
