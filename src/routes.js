58d56b5a3bbf99969c169cdf3bee248f05189f1a
rithikronald <rithikronald29@gmail.com>
2022-08-25 19:24:05 +0530

    Added wallet overview screen

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ const Home = React.lazy(() => import("./views/Home"));
 const CoinDesc = React.lazy(() => import("./views/CoinDesc"));
 const Indexes = React.lazy(() => import("./views/Indexes"));
 const TransactionSummary = React.lazy(() => import('./views/TransactionSummary'))
+const WalletOverview = React.lazy(() => import('./views/WalletOverview'))
+
 
 const routes = [
   { path: "/", name: "Splash", exact: true, component: Splash },
@@ -12,7 +14,8 @@ const routes = [
   { path: "/dashboard", name: "Home", exact: true, component: Home },
   { path: "/coin-desc", name: "CoinDesc", exact: true, component: CoinDesc },
   { path: "/indexes", name: "Indexes", exact: true, component: Indexes },
-  {path: '/transactionSummary', name: 'transactionSummary', exact: true, component: TransactionSummary}
+  {path: '/transactionSummary', name: 'transactionSummary', exact: true, component: TransactionSummary},
+  {path: '/walletOverview', name: 'walletOverview', exact: true, component: WalletOverview  }
 ];
 
 export default routes;
