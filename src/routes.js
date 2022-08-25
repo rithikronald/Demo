f1ec77894764d5038bd9c61fe70bd9309ba5e108
rithikronald <rithikronald29@gmail.com>
2022-08-25 18:14:47 +0530

    Added Transaction Summary Screen

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import React from 'react'
 const Home = React.lazy(() => import("./views/Home"))
 const CoinDesc = React.lazy(() => import('./views/CoinDesc'))
 const Indexes = React.lazy(() => import('./views/Indexes'))
+const TransactionSummary = React.lazy(() => import('./views/TransactionSummary'))
 
 const routes = [
     {path: '/', name: 'Home', exact: true, component: Home},
     {path: '/coin-desc', name: 'CoinDesc', exact: true, component: CoinDesc}, 
-    {path: '/indexes', name: 'Indexes', exact: true, component: Indexes}
+    {path: '/indexes', name: 'Indexes', exact: true, component: Indexes},
+    {path: '/transactionSummary', name: 'transactionSummary', exact: true, component: TransactionSummary}
 ]
 
 export default routes
