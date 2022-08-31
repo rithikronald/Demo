79c5437b2fb3fc63ce1abec86528da7e115934e8
rithikronald <rithikronald29@gmail.com>
2022-08-31 17:12:05 +0530

    Added coin indicators

--- src/views/CoinDesc/index.js
+++ src/views/CoinDesc/index.js
@@ -17,55 +17,46 @@ const data03 = [
     name: "Jan",
     uv: 4000,
     pv: 2400,
-    amt: 2400,
   },
   {
     name: "Feb",
     uv: 3000,
     pv: 1398,
-    amt: 2210,
   },
   {
     name: "Mar",
     uv: 2000,
     pv: 9800,
-    amt: 2290,
   },
   {
     name: "Apr",
     uv: 2780,
     pv: 3908,
-    amt: 2000,
   },
   {
     name: "May",
     uv: 1890,
     pv: 4800,
-    amt: 2181,
   },
   {
     name: "Jun",
     uv: 2390,
     pv: 3800,
-    amt: 2500,
   },
   {
     name: "Jul",
     uv: 3490,
     pv: 4300,
-    amt: 2100,
   },
   {
     name: "Aug",
     uv: 2090,
     pv: 4300,
-    amt: 968,
   },
   {
     name: "Sep",
     uv: 2490,
     pv: 3500,
-    amt: 800,
   },
   {
     name: "Oct",
@@ -129,8 +120,11 @@ const CustomLineChart = (props) => {
     <ResponsiveContainer width={props.width} height={props.height}>
       <LineChart
         data={data03}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        // margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
       >
+         {/* <XAxis style={{fontSize:12}} axisLine={false} /> */}
+         {/* <CartesianGrid strokeDasharray="3 3" stroke="#282929" /> */}
+        {/* <YAxis style={{fontSize:12}} axisLine={false} /> */}
         <Line
           type="monotone"
           dataKey="pv"
@@ -167,9 +161,16 @@ const CoinDesc = () => {
       <div className="flex  h-[30%]">
         <div className="flex basis-1/2 mt-5">
           <div>
+            <div className="flex">
             <p className="text-3xl font-mont text-white font-bold">
               Bitcoin BTC
             </p>
+            <div className="flex space-x-1 ml-2">
+              <div className="w-2 h-2 bg-red-500 rounded-full" />
+              <div className="w-2 h-2 bg-yellow-500 rounded-full" />
+              <div className="w-2 h-2 bg-green-500 rounded-full" />
+            </div>
+            </div>
             <p className="font-mont text-white text-sm mt-8 pr-[40px]">
               Bitcoin is a decentralized digital currency that can be
               transferred on the peer-to-peer bitcoin network. Bitcoin
