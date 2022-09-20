bfe7700b7fe0bca9c3bfaa8721125f41c20bb6c1
Dikshit Upadhya <diksupadhya1234@gmail.com>
2022-09-20 16:54:21 +0530

    Changes made to the coinList flow
    On clicking the ticker or logo or name of coin from the coin list, the
    particular coin description page gets opened.

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,77 +1,82 @@
 import { data } from "autoprefixer";
 import React, { useEffect, useMemo, useState } from "react";
 import DataTable from "react-data-table-component";
+import { useNavigate } from "react-router-dom";
 import { getCoinMeta } from "../../hooks/getcoinMetaData";
 import { FilterComponent } from "./filterComponent";
 
-const columns = [
-  {
-    name: "NAME",
-    selector: (row) => {
-      const coinData = getCoinMeta(row.ticker);
-      return (
-        <div className="flex items-center">
-          <img
-            className="w-6 h-6 rounded-full bg-white"
-            src={coinData?.logoUrl}
-            alt="logo"
-          />
-          <p className="text- font-bold ml-2">{coinData?.ticker}</p>
-          <p className="text-white ml-2">{coinData?.slug}</p>
-        </div>
-      );
-    },
-    sortable: true,
-    grow: 1.5,
-  },
-  {
-    name: "CHANGE",
-    selector: (row) => row.percent_change_24h + "%",
-    sortable: true,
-    style: {
-      color: "#3fa34d",
-      fontWeight: "500",
-    },
-  },
-  {
-    name: "MARKET CAP",
-    selector: (row) => row.marketcap_usd.value,
-    sortable: true,
-    style: {
-      color: "#fff",
-      fontWeight: "500",
-    },
-  },
-  {
-    name: "SUPPLY",
-    selector: (row) => row.total_supply.value,
-    sortable: true,
-    style: {
-      color: "#7d8597",
-      fontWeight: "500",
-    },
-  },
-  {
-    name: "VOLUME",
-    selector: (row) => row.transaction_volume.value,
-    sortable: true,
-    style: {
-      color: "#7d8597",
-      fontWeight: "500",
-    },
-  },
-  {
-    name: "PRICE",
-    selector: (row) => row.price.value,
-    sortable: true,
-    style: {
-      color: "#fff",
-      fontWeight: "500",
-    },
-  },
-];
-
 export const Table = (props) => {
+  const navigate = useNavigate()
+
+  const columns = [
+    {
+      name: "NAME",
+      selector: (row) => {
+        const coinData = getCoinMeta(row.ticker);
+        
+        return (
+          <div className="flex items-center cursor-pointer" onClick={() => navigate(`/coin-desc/${row.ticker}`)}>
+            <img
+              className="w-6 h-6 rounded-full bg-white"
+              src={coinData?.logoUrl}
+              alt="logo"
+            />
+            <p className="text- font-bold ml-2">{coinData?.ticker}</p>
+            <p className="text-white ml-2">{coinData?.slug}</p>
+          </div>
+        );
+      },
+      sortable: true,
+      grow: 1.5,
+    },
+    {
+      name: "CHANGE",
+      selector: (row) => row.percent_change_24h + "%",
+      sortable: true,
+      style: {
+        color: "#3fa34d",
+        fontWeight: "500",
+      },
+    },
+    {
+      name: "MARKET CAP",
+      selector: (row) => row.marketcap_usd.value,
+      sortable: true,
+      style: {
+        color: "#fff",
+        fontWeight: "500",
+      },
+    },
+    {
+      name: "SUPPLY",
+      selector: (row) => row.total_supply.value,
+      sortable: true,
+      style: {
+        color: "#7d8597",
+        fontWeight: "500",
+      },
+    },
+    {
+      name: "VOLUME",
+      selector: (row) => row.transaction_volume.value,
+      sortable: true,
+      style: {
+        color: "#7d8597",
+        fontWeight: "500",
+      },
+    },
+    {
+      name: "PRICE",
+      selector: (row) => row.price.value,
+      sortable: true,
+      style: {
+        color: "#fff",
+        fontWeight: "500",
+      },
+    },
+  ];
+
+
   const [filterText, setFilterText] = useState("");
   const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
 
