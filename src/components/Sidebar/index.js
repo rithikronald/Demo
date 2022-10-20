88998430980f5c9b02546e32c3081b7e4bdb4471
rithikronald <rithikronald29@gmail.com>
2022-10-21 00:32:48 +0530

    Added tag

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -34,14 +34,17 @@ const Sidebar = () => {
           open ? "w-96" : "w-[58px]"
         } background h-screen p-1  pt-8 relative duration-300 sidebar`}
       >
-        <div className="flex gap-x-4 items-center translate-x-1">
+        <div className="flex gap-x-4 items-center translate-x-1  flex-col mr-2">
           <img
             src={Logo}
             className={`cursor-pointer duration-500 ${
               open && "rotate-[360deg]"
             }`}
-            onClick={() => setOpen(!open)}
+            onClick={() => navigate("/dashboard")}
           />
+          <div className="bg-white p-1 mt-2 rounded-md px-1.5">
+            <p className="font-mont font-semibold text-black text-[10px]">Alpha</p>
+          </div>
           <h1
             className={`text-white origin-left font-medium text-xl duration-200 ${
               !open && "scale-0"
@@ -61,7 +64,9 @@ const Sidebar = () => {
           ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"} `}
             >
               <img src={Menu.src} className="" />
-              <span class="absolute z-50 hidden px-6 py-2 left-10 text-center text-white bg-bgl2 rounded tooltip-text group-hover:block">{Menu.title}</span>
+              <span class="absolute z-50 hidden px-6 py-2 left-10 text-center text-white bg-bgl2 rounded tooltip-text group-hover:block">
+                {Menu.title}
+              </span>
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 {Menu.title}
               </span>
