8fdb1a5290bf30a56e562a247e3a7359048ae0c7
rithikronald <rithikronald29@gmail.com>
2022-10-20 21:40:27 +0530

    added getCurrentPrice api from gate

--- src/utility/getCurrentPrice.js
+++ src/utility/getCurrentPrice.js
@@ -2,17 +2,14 @@ import axios from "axios";
 import React from "react";
 
 const host = "http://api.gateio.ws/api/v4/spot/tickers?currency_pair=";
-const headers = {
-  Accept: "application/json",
-  "Content-Type": "application/json",
-};
 
 export const getCurrentPrice = (ticker) => {
   const url = `${host}${ticker}_USDT`;
   console.log("URL", url);
   axios
-    .get(url,{
-        headers
+    .get(url, {
+      Accept: "application/json",
+      "Content-Type": "application/json",
     })
     .then((res) => {
       console.log("GET CURRENT PRICE", res);
