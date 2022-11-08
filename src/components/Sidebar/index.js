a142d5ad5eacf04e45123e9cfab81bd6e21e72a7
Dikshit Upadhya <diksupadhya1234@gmail.com>
2022-11-08 16:25:27 +0530

    Highlghted the menu icon which is currently active

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -16,6 +16,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+
   const Menus = [
     { title: "Dashboard", src: Dashboard, onClick: "/dashboard" },
     { title: "Indexes", src: Indexes, onClick: "/indexes" },
@@ -26,13 +27,19 @@ const Sidebar = () => {
   return noSidebar.includes(location.pathname) ? (
     ""
   ) : (
-    <div style={{zIndex: 999}} className="fixed left-0 top-0 ">
+    <div style={{ zIndex: 999 }} className="fixed left-0 top-0 ">
       <div
         className={` ${
           open ? "w-96" : "w-[58px]"
         } h-screen pt-8 relative duration-300 `}
       >
-        <div className="absolute top-1/2 left-[29px] -translate-y-1/2 h-[450px] w-[55px] rounded-3xl" style={{background: 'linear-gradient(180deg, rgba(91, 27, 114, 0.5) 0%, rgba(167, 76, 239, 0.165) 100%)'}}>
+        <div
+          className="absolute top-1/2 left-[29px] -translate-y-1/2 h-[450px] w-[55px] rounded-3xl"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(91, 27, 114, 0.5) 0%, rgba(167, 76, 239, 0.165) 100%)",
+          }}
+        >
           <div className="flex gap-x-4 items-center translate-x-1 mt-[10px] flex-col mr-2">
             <img
               src={Logo}
@@ -59,13 +66,18 @@ const Sidebar = () => {
               <button
                 onClick={() => navigate(Menu.onClick)}
                 key={index}
-                className={`flex group relative rounded-md p-2 cursor-pointer ${
+                className={`w-[55px] flex group ${location.pathname == Menu.onClick ? "bg-bgl2 w-[40px]" : ""} relative rounded-md p-2 cursor-pointer ${
                   !open ? "mx-auto" : ""
                 } hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-          ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"} w-[55px] `}
+          ${Menu.gap ? "mt-9" : "mt-2"} ${
+                  index === 0 && "bg-light-white"
+                }  `}
               >
                 <img src={Menu.src} className="h-[25px] w-[25px] mx-auto" />
-                <span style={{zIndex: 999}} className="absolute hidden px-6 py-2 left-10 text-center text-white bg-bgl2 rounded tooltip-text group-hover:block">
+                <span
+                  style={{ zIndex: 999 }}
+                  className="absolute hidden px-6 py-2 left-10 text-center text-white bg-bgl2 rounded tooltip-text group-hover:block"
+                >
                   {Menu.title}
                 </span>
                 <span
